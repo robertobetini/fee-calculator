a270c760b1a1927de8d3ae153775e7af6775e907
Roberto Betinho <rbetini@d1.cx>
2022-04-06 01:23:07 -0300

    chore: remove unecessary using directives.

--- Api/Controllers/FeeController.cs
+++ Api/Controllers/FeeController.cs
@@ -1,10 +1,6 @@
 ﻿using Core.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FeeCalculator.Controllers
 {
