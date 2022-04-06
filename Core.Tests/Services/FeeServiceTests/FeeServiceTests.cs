d7dd99b2d82785becd21852d985b977ed928de87
Roberto Betinho <rbetini@d1.cx>
2022-04-06 01:23:19 -0300

    test: add unit tests.

---
+++ Core.Tests/Services/FeeServiceTests/FeeServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Core.Services;
+using Core.Services.Interfaces;
+
+namespace Core.Tests.Services.FeeServiceTests
+{
+    public abstract class FeeServiceTests
+    {
+        protected readonly IFeeService _sut;
+
+        public FeeServiceTests()
+        {
+            _sut = new FeeService();
+        }
+    }
+}
